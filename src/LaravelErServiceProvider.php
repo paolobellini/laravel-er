fa9b0a0ad398191f839b99f8e722f37cc799c61e
Paolo Bellini <paolobellini.web@gmail.com>
2026-02-24 22:26:31 +0100

    initial package setup

---
+++ src/LaravelErServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PaoloBellini\LaravelEr;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelErServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
